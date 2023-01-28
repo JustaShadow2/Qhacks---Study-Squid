fa17ceccaf1e78ae30dfe1dfff6792f25392f438
leongratch <lvgratch@uwaterloo.ca>
2023-01-27 22:55:19 -0500

    Generate reduceable matrice

--- linAlg.py
+++ linAlg.py
@@ -1,22 +1,23 @@
 import random
 
-def Matrix(min, max, isSquare, isAugmented):
+#def Matrix(min, max, isSquare, isAugmented):
+def RREFMatrix():
     #Todo:
         #Implement augmented matrices
         #Implement non-square matrices
     #Current State:
         #Basically just makes matrices that are easy to RREF
-    #min = 2
-    #max = 5
+    min = 2
+    max = 5
     n = random.randint(min, max)
-    #isSquare = True
-    #isAugmented = False
+    isSquare = True
+    isAugmented = False
     if (isSquare):
         m = n
     else:
         while (m == n):
             m = random.randint(min, max)
-    if (isAugmented and (isSquare == False)): 
+    if (isAugmented and (isSquare == False)): #doesnt work rn, too lazy to fix. gonna focus on calclus 
         m = m + 1 #m + 1 matrices so we have augmented stuff woo
     x = [[0 for i in range(n)] for j in range(m)] # x is now a 10x10 array of 0
     for i in range(n-1):
